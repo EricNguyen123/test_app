b92d5eb2dcf053d6f6b75a274a92fbae1eca98af
Your Name <huyhuhyhoa@gmail.com>
2023-12-21 19:17:57 +0700

    login with third party

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,49 +1,42 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def create 
-    user = User.find_by(email: params[:session][:email].downcase) 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
         log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user) 
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to user
       else
-        message = "Account not activated. "
-        message += "Check your email for the activation link." 
+        message = 'Account not activated. '
+        message += 'Check your email for the activation link.'
         flash[:warning] = message
         redirect_to root_url
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
-          # Create an error message.
-      render 'new' 
+      # Create an error message.
+      render 'new'
     end
   end
-  
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
 
-
   def omniauth
-    user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid] , provider: request.env['omniauth.auth'][:provider]) do |user|
+    user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid],
+                                  provider: request.env['omniauth.auth'][:provider]) do |user|
       user.name = request.env['omniauth.auth'][:info][:name]
       user.email = request.env['omniauth.auth'][:info][:email]
-      if user.name.nil?
-        user.name = request.env['omniauth.auth'][:info][:nickname]
-      end
-      if user.email.nil?
-        user.email = user.name + '@gmail.com'
-      end
+      user.name = request.env['omniauth.auth'][:info][:nickname] if user.name.nil?
+      user.email = user.name + '@gmail.com' if user.email.nil?
       user.password = SecureRandom.urlsafe_base64
     end
     if user.valid?
-      if !user.activated?
-        user.activate
-      end
+      user.activate unless user.activated?
       log_in user
       redirect_to root_url
     else
