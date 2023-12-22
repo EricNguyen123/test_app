a871152778f749620314ac30ed9a5008306212c9
Your Name <huyhuhyhoa@gmail.com>
2023-12-22 09:31:44 +0700

    login by google, github, fb

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   
-  get '/auth/github/callback', to: 'sessions#gitauth'
-  get 'auth/facebook/callback', to: 'sessions#facebookauth'
+
   get '/auth/:provider/callback', to: 'sessions#omniauth'
   
 
